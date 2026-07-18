538d4d2329a955f48fcb9b0d4719615270638afa
GervinFung <gervinfungdaxuen@gmail.com>
2026-07-18 19:33:49 +0800

    test(codes): added new test code from `https://github.com/rdebath/Brainfuck/blob/master/testing`

---
+++ test/unmatched-open/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+import TokensGenerator from '../../src/tokens/tokenizer';
+
+describe('Unmatched left bracket', () => {
+    it('should throw for a program with an extra "["', () => {
+        expect(() =>
+            new TokensGenerator('+++++[>+++++++>++<<-]>.>.[').generate()
+        ).toThrowError(/number of brackets/i);
+    });
+});
